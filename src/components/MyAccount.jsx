b82844e8c327d954ee980df9ab7e4f809887ccfb
Alexandra-Diana Ciocan <ciocan_alexandra16@yahoo.com>
2024-05-09 12:46:52 +0300

    added modal form for creating new events

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -1,15 +1,41 @@
-import { Box, Button, Card, CardContent } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  Modal,
+  TextField,
+} from "@mui/material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Typography from "@mui/material/Typography";
 import { observer } from "mobx-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { eventManagerStore } from "../stores/EventManagerStore";
 
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const MyAccount = observer(() => {
+  const [openCreateForm, setOpenCreateForm] = useState(false);
+
   useEffect(() => {
     eventManagerStore.loadEvents();
   }, []);
+
+  const handleCloseCreateForm = () => {
+    setOpenCreateForm(false);
+  };
 
   const loadListItem = (event) => {
     console.log("loading", event);
@@ -53,15 +79,41 @@ const MyAccount = observer(() => {
 
   return (
     <>
-      <Typography variant="h5" component="div">
-        My events:
+      <Typography variant="h5" component="div" paddingTop={5} paddingBottom={3}>
+        My events
       </Typography>
       <List sx={{ width: "100%" }}>
         {eventManagerStore.events
           .filter((event) => event.organizer === eventManagerStore.account)
           .map((event) => loadListItem(event))}
       </List>
-      <Button variant="contained">Create event</Button>
+      <Button variant="contained" onClick={() => setOpenCreateForm(true)}>
+        Create event
+      </Button>
+      <Modal open={openCreateForm} onClose={handleCloseCreateForm}>
+        <Box sx={modalStyle}>
+          <Typography variant="h6" component="h2" paddingBottom={2}>
+            New Event
+          </Typography>
+          <FormControl
+            sx={{ display: "flex", flexDirection: "column", rowGap: 2 }}
+          >
+            <TextField required id="outlined-required" label="Name" />
+            <TextField
+              required
+              id="outlined-required"
+              label="Price"
+              type="number"
+            />
+            <TextField
+              required
+              id="outlined-required"
+              label="Number of tickets"
+              type="number"
+            />
+          </FormControl>
+        </Box>
+      </Modal>
     </>
   );
 });
